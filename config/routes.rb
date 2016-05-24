22ab521edf3788d3e687315cefee8fc162bbd7ba
Lindsey <lindsey.font@gmail.com>
2016-05-24 12:04:05 -0600

    view and create entries

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  root 'entries#index'
+  resources :entries
+
+
+
+  # get 'entries/index'
+
+  # get 'entries/show'
+
+  # get 'entries/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
